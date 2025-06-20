9dc183ec85b5ced41862a462bb7291d3d3c9b6ff
JUned Alam <juned111alam@gmail.com>
2025-06-20 15:38:03 +0530

    this change is made by kunal

--- SpringRest/springrestapp/src/main/java/com/example/springrestapp/SpringrestappApplication.java
+++ SpringRest/springrestapp/src/main/java/com/example/springrestapp/SpringrestappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringrestappApplication {
-// hey
+// thisis the main file of the project 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestappApplication.class, args);
 	}
